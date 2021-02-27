50858511c302f4f10a679af0f20143977fbb82f6
KoenVDZ <koenatwork@gmail.com>
2021-02-27 16:32:14 -0300

    Services Section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,7 +5,7 @@ import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 
 new StickyHeader();
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".services"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 new MobileMenu();
 let modal;
