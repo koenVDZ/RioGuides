c717215819c733e389106cf1c104a5e3ff79f3da
KoenVDZ <koenatwork@gmail.com>
2021-03-01 17:33:54 -0300

    remove modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -8,23 +8,6 @@ new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".services"), 75);
 new RevealOnScroll(document.querySelectorAll(".projects"), 60);
 new MobileMenu();
-let modal;
-
-document.querySelectorAll(".open-modal").forEach((el) => {
-  el.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */ "./modules/Modal")
-        .then((x) => {
-          modal = new x.default();
-          setTimeout(() => modal.openTheModal(), 20);
-        })
-        .catch(() => console.log("There was a problem."));
-    } else {
-      modal.openTheModal();
-    }
-  });
-});
 
 if (module.hot) {
   module.hot.accept();
